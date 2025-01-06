002dff0c835cb1910b7583774c155b4cf11577ff
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2025-01-06 20:28:25 +0100

    Dodanie semafora kontrolującego liczbe pacjentów w budynku przychodni, a także utworzenie struktury pacjenta

--- funkcje.h
+++ funkcje.h
@@ -5,3 +5,16 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <semaphore.h>
+
+#define MAX_PATIENTS 100 // Maksymalna liczba pacjentow do wygenerowania w ciagu dnia
+#define BUILDING_CAPACITY 50 // Maksymalna liczba pacjentow w przychodni
+
+sem_t building_sem; // Semafor kontrolujacy liczbe pacjentow w budynku
+
+
+// Struktura reprezentujaca pacjenta
+typedef struct {
+    int id;
+    int is_vip; // 1 jesli VIP, 0 jesli nie
+    int age;    // Wiek pacjenta
+} Patient;
