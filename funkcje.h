25f258aa2fcd964a9d6adeb0cd699add54d71102
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2025-01-06 20:43:02 +0100

    Dodanie funkcji odpowiadającej za zachowanie procesu pacjenta

--- funkcje.h
+++ funkcje.h
@@ -18,3 +18,24 @@ typedef struct {
     int is_vip; // 1 jesli VIP, 0 jesli nie
     int age;    // Wiek pacjenta
 } Patient;
+
+
+// =============== FUNKCJA OPISUJACA ZACHOWANIE PACJENTA ===============
+
+
+void patient_process(Patient patient) {
+    printf("Pacjent %d (VIP: %d, Wiek: %d) probuje wejsc do budynku.\n", 
+           patient.id, patient.is_vip, patient.age);
+
+    // Proba wejscia do budynku
+    if (sem_trywait(&building_sem) == 0) {
+        printf("Pacjent %d wszedl do budynku.\n", patient.id);
+        sleep(rand() % 3 + 1); // Symulacja czasu spedzonego w budynku
+        printf("Pacjent %d opuszcza budynek.\n", patient.id);
+        sem_post(&building_sem); // Zwalnianie miejsca w budynku
+    } else {
+        printf("Pacjent %d nie mogł wejsc do budynku - brak miejsca.\n", patient.id);
+    }
+
+    exit(0); // Proces pacjenta konczy dzialanie
+}
