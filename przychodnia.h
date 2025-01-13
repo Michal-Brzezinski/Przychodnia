327848128265d9827dbf6a2fab1de8f7ee4feec4
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2025-01-13 23:59:20 +0000

    Stworzenie modelowych funkcji do inspiracji w działaniu przychodni

--- przychodnia.h
+++ przychodnia.h
@@ -1,5 +1,18 @@
 #pragma once
 #include "pacjent.h"
 
+
+// Struktura reprezentująca przychodnię
+typedef struct {
+    int max_patients;  // Maksymalna liczba pacjentów w przychodni
+    int current_patients; // Liczba pacjentów w danej chwili
+    int sem_id;  // Identyfikator zbioru semaforów
+    int queue_id;  // Identyfikator kolejki komunikatów
+
+    // Inne zasoby, jak liczba pacjentów w kolejce do rejestracji, otwarte okienka rejestracji itp.
+} Clinic;
+
 void open_clinic();
 void close_clinic();
+void run_clinic(Clinic *clinic);
+void manage_patients(Clinic *clinic);
