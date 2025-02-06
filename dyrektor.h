50fe7a13231a321b129167d09e764c73e9954249
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2025-02-06 00:41:19 +0000

    Dodanie zarysu pomyslu wykorzystania godzin jako argumentow wywolania funkcji main i konwertowanie ich dzieki sscanf na inty

--- dyrektor.h
+++ dyrektor.h
@@ -13,3 +13,13 @@
 
 void zakonczPraceLekarza(int pid_procesu);
 void nakarzWyjscPacjentom();
+
+int naSekundy(const char *time_str) {
+    int hour, minute;
+    if (sscanf(time_str, "%d:%d", &hour, &minute) != 2) {
+        fprintf(stderr, "Blad: niepoprawny format czasu: %s\n", time_str);
+        exit(1);
+    }
+    return hour * 3600 + minute * 60;   // bo w godzinie jest 3600 sek, a w minucie 60 sek
+}
+
